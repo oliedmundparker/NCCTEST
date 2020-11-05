c4aa5540fa54399f5c993841119b2cec8ada6c32
Oliver Parker <oli.edmund.parker@gmail.com>
2020-11-05 16:18:12 +0000

    Cleans up models, adds string class, adds admin interface for testing.

--- ncctest/vunerabilityscanner/admin.py
+++ ncctest/vunerabilityscanner/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Scan, Scanner, SeverityCounts, Assets, Vulnerability
 
 # Register your models here.
+
+class ScanInline(admin.StackedInline):
+    model = SeverityCounts
+
+
+class ScanAdmin(admin.ModelAdmin):
+    inlines = [ScanInline]
+
+
+admin.site.register(User)
+admin.site.register(Scanner)
+admin.site.register(Assets)
+admin.site.register(Scan, ScanAdmin)
+admin.site.register(Vulnerability)
--- ncctest/vunerabilityscanner/models.py
+++ ncctest/vunerabilityscanner/models.py
@@ -10,11 +10,41 @@ class User(models.Model):
     last_name = models.CharField(max_length=12)
 
     def __str__(self):
-        return self.first_name + self.last_name
+        return "{} {}".format(self.first_name, self.last_name)
 
 
 class Scanner(models.Model):
     name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Assets(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+    created = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+StatusOptions = (
+    ("COMPLETE", "complete"),
+    ("STARTED", "started"),
+    ("NOTSTARTED", "not started")
+)
+
+
+class Scan(models.Model):
+    requested_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    started_at = models.DateTimeField()
+    finished_At = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=StatusOptions)
+    scanners = models.ManyToManyField(Scanner)
+    assets_scanned = models.ManyToManyField(Assets)
+
+    def __str__(self):
+        return "Scan requested by {} at {}".format(self.requested_by, self.started_at)
 
 
 class SeverityCounts(models.Model):
@@ -23,22 +53,7 @@ class SeverityCounts(models.Model):
     medium = models.IntegerField()
     low = models.IntegerField()
     information = models.IntegerField()
-
-
-class Assets(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.CharField(max_length=50)
-    created = models.DateTimeField()
-
-
-class Scan(models.Model):
-    requested_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    started_at = models.DateTimeField()
-    finished_At = models.DateTimeField()
-    status = models.CharField(max_length=20)
-    scanners = models.ManyToManyField(Scanner)
-    severity_count = models.OneToOneField(SeverityCounts, on_delete=models.CASCADE)
-    assets_scanned = models.ManyToManyField(Assets)
+    scan = models.OneToOneField(Scan, on_delete=models.CASCADE)
 
 
 SeverityChoices = (
@@ -59,3 +74,6 @@ class Vulnerability(models.Model):
     references = models.URLField(blank=True)
     cvss_base_scare = models.FloatField()
     affected_assets = models.ManyToManyField(Assets)
+
+    def __str__(self):
+        return self.name
