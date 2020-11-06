b2a07de05bd6c6b866854000c8049588841f4dc1
Oliver Parker <oli.edmund.parker@gmail.com>
2020-11-06 08:34:38 +0000

    Creates views for needed endpoints of API

--- ncctest/vunerabilityscanner/views.py
+++ ncctest/vunerabilityscanner/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .seralizers import ScanSerializer, VulnerabilitySerializer, UserSerializer, AssetsSerializer
+from .models import Scan, Vulnerability, User, Assets
 
 # Create your views here.
+
+
+class ScanViewSet(viewsets.ModelViewSet):
+    queryset = Scan.objects.all().order_by('id')
+    serializer_class = ScanSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('id')
+    serializer_class = UserSerializer
+
+
+class AssetsViewSet(viewsets.ModelViewSet):
+    queryset = Assets.objects.all().order_by('id')
+    serializer_class = AssetsSerializer
+
+
+class VulnerabilityViewSet(viewsets.ModelViewSet):
+    queryset = Vulnerability.objects.all().order_by('id')
+    serializer_class = VulnerabilitySerializer
